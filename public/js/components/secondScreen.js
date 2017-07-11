110346a6c406f85f44cbe65040fa9a008c8bf16f
Misshel <jenmish094@gmail.com>
2017-07-11 05:35:34 -0500

    tamaño del proyecto

--- public/js/components/secondScreen.js
+++ public/js/components/secondScreen.js
@@ -8,7 +8,7 @@ const secondScreen = (update) =>{
   const img = $('<div class="col s4 offset-s4"><img class="responsive-img" src="img/icons/phone.png" alt="celular"></div>');
   const text = $('<div class="col s11 margin-1r"><h1 class="phone-h1 center-align">Para comenzar validemos tu número</h1><p class="phone-p">Resibirás un SMS con un código de validación</p></div>');
   const form = $('<div class="col s12"></div>');
-  const imgIcon = $('<div class="col s3 icon-phone"><img class="responsive-img" src="img/icons/phoneandnumber.png" alt="celular"></div>');
+  const imgIcon = $('<div class="col s3 m1  icon-phone"><img class="responsive-img" src="img/icons/phoneandnumber.png" alt="celular"></div>');
   const inputPhone = $('<input class="col s12 input-phone center-align" name="Phone" value="" minlength="9" maxlength="9"  pattern="[0-9]">');
   const check = $('<input type="checkbox" class="filled-in" id="filled-in-box"/><label for="filled-in-box">Acepto los <a href="#">Terminos y condiciones</a></label>');
   const boton = $('<button id="button" class="col s4 offset-s4 btn btn-secondScreen disabled" type="button" name="button" >CONTINUAR</button>');
